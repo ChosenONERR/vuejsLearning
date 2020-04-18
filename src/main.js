5a221314e577db5e890f973ea21f481f6c6b8c50
chosen <554969463@qq.com>
2020-04-18 23:26:53 +0800

    学习webpack另一个阶段哦

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-// 这是main.js ，是我们项目的JS入口文件。这个文件存在的理由如下：
-// 因为 我们不推荐直接在每个页面上面引入任何包和css文件（这样会导致每个页面都有多次二次ajax请求）
-// 我们推荐所有的东西都在main.js上面写，以后每个页面只要引入main.js文件就可以了
+/*
+这是main.js ，是我们项目的JS入口文件。这个文件存在的理由如下：
+因为 我们不推荐直接在每个页面上面引入任何包(js)和css文件（这样会导致每个页面都有多次二次ajax请求）
+我们推荐所有的东西都在main.js上面写，以后每个页面只要引入main.js文件就可以了
+*/
 
 // 1.导入包 如jquery
 // es6中导入模块的格式： import 接收变量名 from 具体包名
@@ -8,10 +10,30 @@
 // 下面通过命令用webpack对main.js进行处理：
 //命令格式： webpack 要打包的文件的路径 -o 打包好的文件的路径
 // webpack ./webpack_learning/src/main.js -o ./webpack_learning/dist/bundle.js
-import $ from 'jquery'
+import $ from 'jquery' // 引入一个js包，我们需要用一个变量接受一下（不同于css// ）
 // const $ = require('jquery') // 这是node.js中引入包的写法（不经过webpack处理浏览器也解析不了）
+
+import './css/index.css' // 引入一个css样式表
+import './css/index.less' // 引入.less文件
+import './css/index.scss' // 引入.scss文件
+/*
+注意：webpack 默认只能打包处理 js类型的文件，无法处理其他非 js类型的文件
+如果非要处理，必须安装一些合适的第三方loader加载器：
+1. 如果想要打包 css文件，需要安装两个插件： npm i style-loader css-loader -D
+2. 打开 webpack.config.js 这个配置文件新增一个配置节点 module，它是一个对象。 在modiule对象身上有一个rules数组属性，
+   存放了所有第三方文件的匹配和处理规则
+3. .less文件同理，需要下载插件 npm i less-loader -D （P.S.：在这之前得先 nom i less -D 因为该插件依赖它）
+
+注意： webpack 处理第三方文件类型的过程：
+1. webpack发现这个  要处理的的文件不是js文件，然后就去配置文件webpack.config.js中查找有无对应的第三方的loader规则
+2. 如果能找到对应的规则，就会调用对应的loader处理这种文件类型
+3. 在调用loader的时候，是从后往前调用的（例：如若第三方匹配规则是：{test: /\.css$/, use: ['style-loader', 'css-loader']}，
+   则先调用css-loader，调用的结果传给style-loader）
+4. 当最后一个loader调用完毕会把最后的处理结果直接交给webpack进行打包合并，最终输出到bundle.js文件中去
+*/
+
 $(function () {
-    $('li:odd').css('backgroundColor', 'lightblue') // 选择器选中li的奇数行
+    $('li:odd').css('backgroundColor', 'blue') // 选择器选中li的奇数行
     $('li:even').css('backgroundColor', function () {
         return '#' + 'D97634'
     }) // 选择器选中li的偶数行
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 // webpack.config.js 是webpack的配置文件，这个文件应该放在项目的根目录下
-
+// 由于 webpack 是基于Node进行构建的，所以webpack配置文件中任何合法的的Node代码都是支持的
 const path = require('path')
+
+// 启用热更新的第 2 步：导入局部安装的webpack
+const webpack = require('webpack')
+
+// 导入 可以在内存中生成 html页面的插件：html-webpack-plugin （通过 npm i html-webpack-plugin -D 安装
+// 这个插件的作用：
+// 1. 自动在内存中根据指定的物理磁盘上的页面生成一个一摸一样的在内存的页面
+// 2. 自动把打包好的、在内存中的bundle.js 追加到内存中去
+const htmlWebpackPlugin = require('html-webpack-plugin') // ps：只要是插件都要放到 plugins 数组里面去
 
 
 // 该配置文件是一个 .js文件，可以通过 Node 中的模块操作向外暴露一个 “配置对象”
@@ -21,14 +30,47 @@ module.exports = {
     // 指定 打包的入口
     entry: './src/main.js', // 指定 将要被打包的文件    ./src/main.js
     // 指定 打包的出口
-    output:{
+    output: {
         path: path.resolve(__dirname, './dist'), // 指定 打包好的文件 将要被输出的目录位置    ./dist
         filename: 'bundle.js' // 指定 输出文件的名称
+    },
+    devServer: { // 这是配置 dev-server 命令参数的第二种方式（第一种方式在package.json文件中配置），这种方式麻烦一点
+        open: true, // 打包完成后自动打开浏览器
+        port: 9090, // 设置启动时候的运行端口
+        contentBase: 'src', // 指定托管的根目录
+        hot: true // 启用热更新的 第 1 步
+    },
+    plugins: [ // 该节点用于配置插件
+        new webpack.HotModuleReplacementPlugin(), // new 一个热更新的 模块对象，这是启用热更新的第 3 步
+        new htmlWebpackPlugin({ // 创建一个 可以在内存中生成 html页面 的插件
+            template: path.join(__dirname, './src/index.html'), // 指定模板页面，将来就将这个页面在内存中生成一份
+            filename: "index.html" //指定内存中生成的页面的名称
+        })
+    ],
+    module: { // 该节点用于配置所有 第三方模块加载器
+        rules: [ //所有第三方模块的 匹配规则
+            // 采用正则表达式匹配main.js文件中引入的文件：
+            // 1.配置处理 .css文件中的第三方loader规则
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            // 2.配置处理 .less文件的第三方 loader规则
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+            // 3.配置处理 .scss文件的第三方loader规则
+            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+            // 4.配置处理 图片路径文件的第三饭loader规则（P.S.：不需要配置file-loader，因为这是内部被依赖的）
+            //   我们可以给loader传递命令参数，传参的格式和url传值的格式一致，如 http://XXX?id=10&name=zhangsan
+            //   我们发现，使用这个之后，页面图片被默认转成了base64编码，但我们希望大图片不要转成这种编码
+            //  （1）limit 给定的值是图片的大小，单位是byte，如果我们引用的图片大于或等于给定的limit值，则不会被转为base64；
+            //  （2）使用 limit之后，发现图片是正常的形式，不再是base64编码了，但是名字却是一个32为的hash值。之所以这样，
+            //       是因为它为了让名字不重名（重名会导致后一张图片替换掉前一张图片的现象）。这时候我们希望 图片的名字
+            //       具有可读性，又不会重名。可以传递name参数，name参数的组成中包括 8位hash值 + 图片原来的名字+后缀名
+            {test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=251616&name=[hash:8]-[name].[ext]'},
+        ]
     }
 }
 
 /* 小结：
-* 当我们使用命令'webpack'的时候，webpack发现我们并没有用规定的命令给它指定入口和出口，故webpack将执行下列步骤：
+* 当我们使用命令'webpack'或'webpack-dev-server'的时候，webpack发现我们并没有用规定的命令格式给它指定入口和出口，
+* 故webpack将执行下列步骤：
 * 1. webpack 先去项目的根目录下查找有没有 webpack.config.js 配置文件
 * 2. 当找到配置文件之后， webpack就会去解析它，结胸完成之后就会得到一个配置对象，并通过node到处该配置对象
 * 3. 当 webpack 拿到配置对象之后，就知道了打包的入口和出口
